25e789aef3f2e5a713f630cf6daff2acb55d5c5c
Ruth <raiynegicheha@gmail.com>
2023-11-04 04:05:34 +0300

    Create Quiz.js

---
+++ quizzical/src/Quiz.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import Questions from './Questions';
+
+
+
+export default function Quiz() {
+
+   
+
+  return (
+
+    <div></div>
+
+  )
+}
