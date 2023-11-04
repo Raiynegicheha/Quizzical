5bef596a97d0839ea35cb810e9e1648329ccbe69
Ruth <raiynegicheha@gmail.com>
2023-11-04 04:05:42 +0300

    Update App.js

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import Questions from './Questions';
+import
+
+
 
 function App() {
+  const [quizStart, setQuizStart] = useState(true);
+
+  function startQuiz (){
+      setQuizStart(!quizStart)
+  }
+
+
   return (
-    <div className="App">
-      
+    <div>
+        {quizStart? (<Questions />) :
+        <div>
+        <div className = "quizzical">
+        <div className= "align">
+        <h1 className="heading">Quizzical</h1>
+        <p className= "title">React Test</p>
+        <button className="start" onClick={startQuiz}>Start Test</button>
+        </div>
+    
+    </div>
+    </div>
+        }
+
+
+
     </div>
   );
 }
