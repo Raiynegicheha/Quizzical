a57dc554239119d69bb4cd5d8594f83a71686b32
Ruth <raiynegicheha@gmail.com>
2023-11-04 04:05:38 +0300

    Create Questions.js

---
+++ quizzical/src/Questions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Questions() {
+  return (
+    <div className='questions'>
+        <h1 className='title'>React</h1>
+        <label>What is react</label>
+        <input type='radio'></input>
+    </div>
+  )
+}
