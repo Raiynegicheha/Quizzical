470ec7dcfbdbe736769b01e83b2464cc519416f1
Ruth <raiynegicheha@gmail.com>
2023-11-07 15:16:07 +0300

    Update App.js

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Questions from './Questions';
-import
+import { useState } from 'react';
+import Quiz from './Quiz';
 
 
 
-function App() {
-  const [quizStart, setQuizStart] = useState(true);
+export default function App() {
+  const [quizStart, setQuizStart] = useState(false);
 
   function startQuiz (){
       setQuizStart(!quizStart)
@@ -13,24 +14,23 @@ function App() {
 
 
   return (
-    <div>
-        {quizStart? (<Questions />) :
-        <div>
-        <div className = "quizzical">
-        <div className= "align">
-        <h1 className="heading">Quizzical</h1>
-        <p className= "title">React Test</p>
-        <button className="start" onClick={startQuiz}>Start Test</button>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f5f5f5' }}>
+        <div style={{ backgroundColor: '#fff', borderRadius: '5px', padding: '20px', boxSizing: 'border-box', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {quizStart? (<Quiz />) :
+            <div>
+            <div className = "quizzical" >
+            <div className= "align">
+            <h1 className="heading">Quizzical</h1>
+            <p className= "title">Some description if needed</p>
+            <button className="start" onClick={startQuiz}>Start Test</button>
+            </div>
+        
+            </div>
+            </div>
+            }
         </div>
-    
-    </div>
-    </div>
-        }
-
-
-
     </div>
   );
 }
 
-export default App;
+
