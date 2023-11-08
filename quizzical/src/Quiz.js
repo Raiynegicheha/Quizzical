97866dfcbdf86b6ee593aac1beb528bf0bdb4de2
Ruth <raiynegicheha@gmail.com>
2023-11-08 15:54:31 +0300

    Update Quiz.js

--- quizzical/src/Quiz.js
+++ quizzical/src/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import Questions from './Questions'
 // import { useState } from 'react'
 // import Questions from './Questions';
@@ -6,13 +6,46 @@ import Questions from './Questions'
 
 
 export default function Quiz() {
+  const[quizData, setQuizData] = useState([])
+
+  const fetchQuizData = async () => {
+    try{
+      const response = await fetch("https://opentdb.com/api.php?amount=5&category=12&difficulty=easy")
+      const data = await response.json()
+      setQuizData(data.results)
+    }
+    catch(error){
+      console.error("Error fetching Data", error)
+    }
+  }
+  // console.log(fetchQuizData());
+
+  useEffect(()=>{
+    fetchQuizData()
+  },[])
+
+
+
+  
+
+  
 
    
 
   return (
 
     <div>
-      <Questions />
+      <h1>Quiz Test</h1>
+      {quizData && quizData.map((result, index)=>( 
+      
+      <Questions
+        key = {index}
+        index = {index}
+        question = {result.question}
+        correctAnswer = {result.correct_answer}
+        incorrectAnswer = {result.incorrect_answers}
+
+      />))}
     </div>
 
   )
